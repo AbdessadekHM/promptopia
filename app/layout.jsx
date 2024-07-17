daa774a5213e2af220e1568de22210ad8fd370d3
Abdessadek Hmiddouch <abdessadekhm@gmail.com>
2024-07-17 19:10:13 +0100

    Set Auth Config

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,7 @@
 import Nav from "@/components/Nav"
+
+
+import Provider from "@/components/Provider"
 import "@/styles/globals.css"
 
 export const metadata = {
@@ -9,13 +12,15 @@ const RootLayout = ({children}) => {
   return (
     <html lang="en">
         <body>
-            <div className="main">
-                <div className="gradient"/>
-            </div>
-            <main className="app">
-                <Nav/>
-                {children}
-            </main>
+            <Provider>
+                <div className="main">
+                    <div className="gradient"/>
+                </div>
+                <main className="app">
+                    <Nav/>
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
