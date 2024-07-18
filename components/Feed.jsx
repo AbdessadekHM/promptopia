8a4bc005907fff833c3608c16e7667ccf61e49df
Abdessadek Hmiddouch <abdessadekhm@gmail.com>
2024-07-18 19:36:14 +0100

    Add Prompts Posts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+'use client'
+
+import {useState, useEffect} from 'react'
+import PromptCard from './PromptCard'
+
+
+const PromptCardList = ({data, handleClick})=>{
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post)=>{
+       return <PromptCard 
+        key={post._id} 
+        post={post} 
+        handleClick={handleClick}
+       /> 
+      })}
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) =>{
+
+  }
+  useEffect(()=>{
+    const fetchPosts = async ()=>{
+      try {
+        const response = await fetch('/api/prompt');
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+       console.log("error",error) 
+      }
+    }
+    fetchPosts();
+  },[]);
+  
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form action="" className='relative w-full flex-center'>
+        <input 
+          type="text" 
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+      <PromptCardList 
+        data={posts}
+        handleClick={()=>{}}
+      />
+    </section>
   )
 }
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+'use client'
+import {useState} from 'react'
+import Image from 'next/image'
+import { useSession } from 'next-auth/react'
+import { usePathname, useRouter } from 'next/navigation'
+const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
+  const [copied, setCopied] = useState("");
 
-const PromptCard = () => {
+  const handleCopy =  ()=>{
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(()=>setCopied(''),3000);
+  }
   return (
-    <div>PromptCard</div>
+    <div className="prompt_card">
+      <div className='flex items-start justify-between gap-5'>
+        <div className='flex items-center justify-start flex-1 gap-3 cursor-pointer'>
+          <Image 
+            src={post.creator.image}
+            alt='user_image'
+            width={40}
+            height={40}
+            className='object-contain rounded-full'
+          />
+          <div className='flex flex-col'>
+            <h3 className='font-bold text-gray-900 font-satoshi'>
+              @{post.creator.username}
+              <p className='text-sm text-gray-500 font-inter'>
+                {post.creator.email}
+              </p>
+            </h3>
+          </div>
+        </div>
+        <div className='copy_btn' onClick={handleCopy}>
+          <Image
+            src={copied === post.prompt
+              ? '/assets/icons/tick.svg'
+              : '/assets/icons/copy.svg'
+            }
+            alt='copy'
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className='my-4 text-sm text-gray-700 font-satoshi '>
+        {post.prompt}
+        
+      </p>
+      <p className='text-sm cursor-pointer font-inter blue_gradient'
+        onClick={()=>handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+    </div>
   )
 }
 
